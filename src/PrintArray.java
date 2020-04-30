be69406cbca6b45f4ae43a8152c7f8caec979a3f
Kiran607 <ickirankiru@gmail.com>
2020-04-30 23:37:50 +0530

    [Kiran]Adding generics in class declaration

--- src/PrintArray.java
+++ src/PrintArray.java
@@ -1,13 +1,23 @@
-public class PrintArray {
+public class PrintArray <E> {
+
+    E[] myArray;
+
+    public PrintArray(E[] myArray) {
+        this.myArray = myArray;
+    }
 
     public static void main(String[] args) {
         Integer[] a = {1, 2, 3};
         Double[] b = {1.1, 2.2, 3.3};
         Character[] c = {'a', 'b', 'c'};
-        toPrint(a);
-        toPrint(b);
-        toPrint(c);
+        new PrintArray<Integer>(a).toPrint();
+        new PrintArray<Double>(b).toPrint();
+        new PrintArray<Character>(c).toPrint();
     }
+    private void toPrint(){
+        toPrint(myArray);
+    }
+
     private  static <E> void toPrint(E[] a) {
         for (E value:a)
             System.out.println(value);
