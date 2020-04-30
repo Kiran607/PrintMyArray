182d7427949b39e589d13e890377ae385657f04d
Kiran607 <ickirankiru@gmail.com>
2020-04-30 23:20:54 +0530

    [Kiran]Used generics in methods to print array

--- src/PrintArray.java
+++ src/PrintArray.java
@@ -1,25 +1,15 @@
 public class PrintArray {
+
     public static void main(String[] args) {
-        int[] a = {1, 2, 3};
-        double[] b = {1.1, 2.2, 3.3};
-        char[] c = {'a', 'b', 'c'};
+        Integer[] a = {1, 2, 3};
+        Double[] b = {1.1, 2.2, 3.3};
+        Character[] c = {'a', 'b', 'c'};
         toPrint(a);
         toPrint(b);
         toPrint(c);
     }
-
-    public static void toPrint(int[] a) {
-        for(int myArray:a)
-        System.out.println(myArray);
-    }
-
-    public static void toPrint(double[] b) {
-        for (double myArray:b)
-        System.out.println(myArray);
-    }
-
-    public static void toPrint(char[] c) {
-        for (char myArray:c)
-            System.out.println(myArray);
+    private  static <E> void toPrint(E[] a) {
+        for (E value:a)
+            System.out.println(value);
     }
 }
